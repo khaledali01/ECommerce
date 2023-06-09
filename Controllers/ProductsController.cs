a8cfb912d0e4812fa4607b6cc865be2973e0f345
Khaled <khaled.ali.architect@gmail>
2023-06-09 18:17:59 +0300

    Creating API Controllers
    See: #5

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.Data.Entities;
+using ECommerce.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    [ApiController]
+	[Route("api/[controller]")]
+
+	public class ProductsController : ControllerBase
+	{
+		private readonly ECommerceContext _context;
+
+		public ProductsController(ECommerceContext context)
+		{
+			this._context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			return await _context.Products.ToListAsync();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return product;
+		}
+	}
+}
