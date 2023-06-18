4fbb140d9a169ff0d81d02f6854cd797ad6adb7d
Khaled <khaled.ali.architect@gmail>
2023-06-18 21:52:20 +0300

    Creating the Login Page
    See: #17

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,16 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductListingComponent } from './product-listing/product-listing.component';
 import { ProductPageComponent } from './product-listing/product-page/product-page.component';
-import { ApiService } from './services/api.service';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
-
 const routes: Routes = [
-  { path: '', component: ProductListingComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: ':id', component: ProductPageComponent },
+  { path: '', component: ProductListingComponent },
 ];
 
 @NgModule({
@@ -31,6 +30,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
