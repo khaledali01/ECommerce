7fe0ea2922b1b9f919faf9300209b7458cfa3308
Khaled <khaled.ali.architect@gmail>
2023-06-18 17:33:13 +0300

    Modifying Product Repository Name in the Product Controller
    See: #14

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -9,24 +9,24 @@ namespace ECommerce.Controllers
 
 	public class ProductsController : ControllerBase
 	{
-        private readonly IGenericRepository<Product> _gRepository;
+        private readonly IGenericRepository<Product> _productRepository;
 
         public ProductsController(IGenericRepository<Product> gRepository)
 		{
-            _gRepository = gRepository;
+            _productRepository = gRepository;
         }
 
 		[HttpGet]
 		public async Task<IActionResult> GetProducts()
 		{
-			var res = await _gRepository.GetAll();
+			var res = await _productRepository.GetAll();
 			return Ok(res);
 		}
 
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetProduct(int id)
 		{
-			var product = await _gRepository.GetById(id);
+			var product = await _productRepository.GetById(id);
 
 			if (product == null)
 			{
